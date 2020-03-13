ecebd1ccc68b90bf471f6bd13b658a5ceb2904dc
sancognizant <e0227472@u.nus.edu>
2020-03-13 15:42:39 +0800

    added first test case

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+export interface propTypes {
+    styles: any; text: string; onClick: any;
+}
 
-const Button = ({styles,text,onClick}: {styles: any, text: string, onClick: any}) => {
+const Button = ({styles,text,onClick}: propTypes) => {
     return (  
      <button 
+     data-testid = "button"
      type="button" 
      className = {styles}
      onClick = {onClick}>{text}</button>
