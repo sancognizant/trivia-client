d9e05cc22c2b255c376af5cc082b04c0deb3e5b2
sancognizant <e0227472@u.nus.edu>
2020-03-16 18:40:27 +0800

    added form part 1

--- src/components/quiz/quizInterface.tsx
+++ src/components/quiz/quizInterface.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Counter from './counter';
-import { questions } from '../../utils/questionLoader';
 import '../../scss/buttonStyle.scss';
 import Button from '../button';
+import { questions } from '../../utils/questionLoader';
+import { any } from 'prop-types';
 
 /*
 questionList: set the total number of questions from api call 
 questionNumber: current question that user is attempting 
 */
-
-interface questionsListInter {
+export interface questionsListInter {
 	question: string,
 	choices: string[],
 	answer: number
@@ -20,29 +20,54 @@ interface questionNumberInter {
 }
 
 const Quiz = () => {
-	const [ questionsList, setQuestionsList ] = useState<questionsListInter[]>([]);
-	const [ questionNumber, setQuestionNumber ] = useState<questionNumberInter>({currentQuestion: 1});
+	const [questionsList, setQuestionsList] = useState<questionsListInter[]>([]);
+
+	const [questionNumber, setQuestionNumber] = useState<questionNumberInter>({ currentQuestion: 1 });
+
+	const [alias, setAlias] = useState("");
 
 	useEffect(
 		() => {
 			setQuestionsList(questions);
 		},
-		[ questions ]
+		[questions]
 	);
 
 	const handleIncrementNumber = () => {
-		setQuestionNumber({currentQuestion: questionNumber.currentQuestion+ 1});
-    };
-    
-	/*
+		setQuestionNumber({ currentQuestion: questionNumber.currentQuestion + 1 });
+	};
+
+	const handleFormSubmit = (e: any) => {
+		console.log(alias);
+		handleIncrementNumber();
+		e.preventDefault();
+	}
+
+
+
+    /*
 render counter only if the number of questions have been set correctly from the api
     */
 	return (
-		<div data-testid = "quiz-container">
+		<div data-testid="quiz-container">
 			{questionsList.length !== 0 ? (
-				<div data-testid = "quiz-component">
-				<Counter totalNumber={questionsList.length} activeNumber={questionNumber.currentQuestion} />
-				<Button text="submit" styles="back" onClick={handleIncrementNumber} />
+				<div data-testid="quiz-component">
+					<Counter totalNumber={questionsList.length} activeNumber={questionNumber.currentQuestion} />
+					<div className="questionComponent">
+						<form onSubmit={handleFormSubmit}>
+							<div className="User">
+								<label>Username</label>
+								<input
+									type="text"
+									name="name"
+									onChange={event => setAlias(event.target.value)}
+									value={alias}
+									placeholder="Please enter name/alias"
+								/>
+							</div>
+						</form>
+					</div>
+					<Button text="submit" styles="back" onClick={handleFormSubmit} />
 				</div>
 			) : null}
 		</div>
